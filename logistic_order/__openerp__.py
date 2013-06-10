57a5edd2d178a30e699fbd25e0152978cadf5eca
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-06-10 09:35:45 +0200

    [ADD] skeleton of logistic_order addon

---
+++ logistic_order/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more description.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{"name": "Logistic Order",
+ "version": "0.1",
+ "author": "OpenERP SA",
+ "license": "AGPL-3",
+ "category": "Purchase Management",
+ 'complexity': "normal",
+ "images": [],
+ "website": "http://www.openerp.com",
+ "description": """
+This module customizes the Sales Orders to disguise them in Logistic Orders
+===========================================================================
+
+""",
+ "depends": ["sale",
+             ],
+ "demo": [],
+ "data": [],
+ "auto_install": False,
+ "test": [],
+ "installable": True,
+ }
