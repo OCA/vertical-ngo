5ea8050303071094afc703abec36b06f623ee948
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-06-19 16:05:19 +0200

    [CHG] remove the field preferred_sourcing from logistic.requisition

--- logistic_requisition/model/logistic_requisition.py
+++ logistic_requisition/model/logistic_requisition.py
@@ -81,11 +81,6 @@ class logistic_requisition(orm.Model):
             'res.partner', 'Delivery Address', required=True,
             states=REQ_STATES
         ),
-        'preferred_sourcing': fields.selection(
-            [('procurement', 'Procurement'),
-             ('wh_dispatch', 'Warehouse Dispatch')],
-            string='Preferred Sourcing'
-        ),
         'country_id': fields.related(
             'consignee_shipping_id',
             'country_id',
