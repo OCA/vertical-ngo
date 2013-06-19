70914f80e72c1c1c3d5e341b87e6302084cdd8ec
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-06-19 11:50:18 +0200

    [CHG] the knowledge of the cost estimate's creation is now in a wizard, it can create from a requisition or from lines

--- logistic_requisition/model/stock.py
+++ logistic_requisition/model/stock.py
@@ -6,22 +6,27 @@ from openerp.osv import orm
 class stock_location(orm.Model):
     _inherit = "stock.location"
 
-    def name_get(self, cr, uid, ids, context=None):
-        # always return the full hierarchical name
-        res = self._complete_name(cr, uid, ids,
-                                  'complete_name',
-                                  None,
-                                  context=context)
-        return res.items()
+    def _get_shop_from_location(self, cr, uid, location_id, context=None):
+        """ Returns the sale.shop for a location.
 
-    def _complete_name(self, cr, uid, ids, name, args, context=None):
-        """ Forms complete name of location from parent location to
-        child location.
-
-        :returns: Dictionary of values
+        Returns None if no shop exist for a location.
         """
-        res = {}
-        # TODO
-        for m in self.browse(cr, uid, ids, context=context):
-            res[m.id] = m.name
-        return res
+        if isinstance(location_id, (tuple, list)):
+            assert len(location_id) == 1, "Only 1 ID accepted"
+            location_id = location_id[0]
+        warehouse_obj = self.pool.get('stock.warehouse')
+        shop_obj = self.pool.get('sale.shop')
+        warehouse_ids = warehouse_obj.search(
+            cr, uid,
+            [('lot_stock_id', '=', location_id)],
+            context=context)
+        if not warehouse_ids:
+            return None
+        shop_ids = shop_obj.search(cr, uid,
+                                   [('warehouse_id', 'in', warehouse_ids)],
+                                   context=context)
+        if not shop_ids:
+            return None
+        assert len(shop_ids) == 1, (
+            "Several shops found for location with id %s" % location_id)
+        return shop_ids[0]
