d404a2d81d668aa87942b86677a7aae65501df65
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-06-25 13:28:15 +0200

    [IMP] Add incoterm name_get function to have code - (name)

--- logistic_requisition/model/stock.py
+++ logistic_requisition/model/stock.py
@@ -30,3 +30,30 @@ class stock_location(orm.Model):
         assert len(shop_ids) == 1, (
             "Several shops found for location with id %s" % location_id)
         return shop_ids[0]
+
+class stock_incoterms(orm.Model):
+    _inherit = "stock.incoterms"
+    
+    def name_get(self, cr, user, ids, context=None):
+        """
+        Returns a list of tupples containing id, name.
+        result format: {[(id, name), (id, name), ...]}
+
+        @param cr: A database cursor
+        @param user: ID of the user currently logged in
+        @param ids: list of ids for which name should be read
+        @param context: context arguments, like lang, time zone
+
+        @return: Returns a list of tupples containing id, name 
+                 composed by code + name
+        """
+        if not ids:
+            return []
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        result = self.browse(cr, user, ids, context=context)
+        res = []
+        for rs in result:
+            name = "%s - (%s)" % (rs.code, rs.name)
+            res += [(rs.id, name)]
+        return res
