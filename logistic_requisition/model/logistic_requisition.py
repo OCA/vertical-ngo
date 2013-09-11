a3d5ea4506dc175f951e514cb0b241c6bf4759d8
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2013-09-11 15:00:38 +0200

    [ADD] default_source_address field on logistic source line. The field do a lookup for the source address based on procurment method

--- logistic_requisition/model/logistic_requisition.py
+++ logistic_requisition/model/logistic_requisition.py
@@ -819,6 +819,19 @@ class logistic_requisition_source(orm.Model):
             result[line.id] = po_lines[0].id if po_lines else False
         return result
 
+    def _default_source_address(self, cr, uid, ids, field_name, arg, context=None):
+        """Return the default source address depending of the procurment method"""
+        res = {}
+        for line in self.browse(cr, uid, ids, context=context):
+            res[line.id] = False
+            if line.procurement_method == 'wh_dispatch':
+                loc_id = line.location_partner_id.id if line.location_partner_id else False
+                res[line.id] = loc_id
+            else:
+                sup_id = line.supplier_partner_id.id if line.supplier_partner_id else False
+                res[line.id] = sup_id
+            return res
+
     _columns = {
         'name': fields.char('Source Name', readonly=True),
         'requisition_line_id': fields.many2one(
@@ -950,6 +963,11 @@ class logistic_requisition_source(orm.Model):
             'dispatch_location_id', 'partner_id',
             type='many2one', relation='res.partner',
             string='Location Address', readonly=True),
+        'default_source_address': fields.function(_default_source_address,
+                                                  type='many2one',
+                                                  relation='res.partner',
+                                                  string='Default source',
+                                                  readonly=True)
     }
 
     _defaults = {
