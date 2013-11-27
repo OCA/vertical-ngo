31113e5dd17c617fb599a96ffb1fc036a10f0f8e
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2013-11-27 13:38:00 +0100

    [IMP] source line price is now = PO HT total

--- framework_agreement_sourcing/model/logistic_requisition_cost_estimate.py
+++ framework_agreement_sourcing/model/logistic_requisition_cost_estimate.py
@@ -30,9 +30,32 @@ class logistic_requisition_cost_estimate(orm.Model):
     def _update_agreement_source(self, cr, uid, source, context=None):
         """Update price of source line using related confirmed PO"""
         if source.procurement_method == AGR_PROC:
+            self._link_po_lines_to_source(cr, uid, source, context=context)
             price = source.get_agreement_price_from_po()
             source.write({'unit_cost': price})
             source.refresh()
+
+    def _link_po_lines_to_source(self, cr, uid, source, context=None):
+        po_l_obj = self.pool['purchase.order.line']
+        agr = source.framework_agreement_id
+        line_ids = po_l_obj.search(
+            cr, uid,
+            [('order_id.framework_agreement_id', '=', agr.id),
+             ('lr_source_line_id', '=', source.id),
+             ('order_id.partner_id', '=', agr.supplier_id.id)],
+            context=context)
+        lines = po_l_obj.browse(cr, uid, line_ids, context=context)
+        po_ids = ([line.order_id.id for line in lines])
+
+        all_line_ids = po_l_obj.search(
+            cr, uid,
+            [('order_id', 'in', po_ids)],
+            context=context)
+
+        po_l_obj.write(cr, uid, all_line_ids,
+                       {'lr_source_line_id': source.id},
+                       context=context)
+        source.refresh()
 
     def _prepare_cost_estimate_line(self, cr, uid, sourcing, context=None):
         """Override in order to update agreement source line
--- framework_agreement_sourcing/model/purchase.py
+++ framework_agreement_sourcing/model/purchase.py
@@ -82,3 +82,10 @@ class purchase_order(orm.Model, BrowseAdapterMixin):
         if callable(post_fun):
             post_fun(cr, uid, l_id, origin, context=context)
         return l_id
+
+    def action_confirm(self, cr, uid, ids, context=None):
+        super(purchase_order_line, self).action_confirm(cr, uid, ids, context=context)
+        for element in self.browse(cr, uid, ids, context=context):
+            if not element.quantity_bid and not element.framework_agreement_id:
+                self.write(cr, uid, ids, {'quantity_bid': element.product_qty}, context=context)
+        return True
