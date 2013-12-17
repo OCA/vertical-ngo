b849c3827445704b93ac3da99975f3367840e97c
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2013-12-17 15:27:29 +0100

    [FIX] LR origin not put on Coste estimate origin field

--- logistic_requisition/wizard/cost_estimate.py
+++ logistic_requisition/wizard/cost_estimate.py
@@ -243,6 +243,7 @@ class logistic_requisition_cost_estimate(orm.TransientModel):
                 'incoterm': requisition.incoterm_id.id,
                 'incoterm_address': requisition.incoterm_address,
                 'requisition_id': requisition.id,
+                'origin': requisition.name,
                 'project_id': requisition.analytic_id.id if requisition.analytic_id else False,
                 }
 
