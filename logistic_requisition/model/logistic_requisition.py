6ed0d306f6d7673557fabd2e9165a1f0a082dc21
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2013-12-17 13:06:35 +0100

    [FIX] propagation of incoterm on tender form LR

--- logistic_requisition/model/logistic_requisition.py
+++ logistic_requisition/model/logistic_requisition.py
@@ -1090,6 +1090,8 @@ class logistic_requisition_source(orm.Model):
                 'dest_address_id': dest_address_id,
                 'line_ids': [(0, 0, rline) for rline in purch_req_lines],
                 'origin': ", ".join(origin),
+                'req_incoterm_id': line.requisition_id.incoterm_id.id,
+                'req_incoterm_address': line.requisition_id.incoterm_address,
                 }
 
     def _prepare_po_requisition_line(self, cr, uid, line, context=None):
