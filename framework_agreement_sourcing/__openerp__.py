d06d0cd98001079eadf7ec056330940fe2ef51e0
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-02-04 11:09:08 +0100

    [ADD] security for po creator

--- framework_agreement_sourcing/__openerp__.py
+++ framework_agreement_sourcing/__openerp__.py
@@ -55,6 +55,7 @@ the same time in company.
  'data': [
     'view/requisition_view.xml',
     'wizard/logistic_requisition_source_create_po_view.xml',
+    'security/ir.model.access.csv'
  ],
  'demo': [],
  'test': [],
