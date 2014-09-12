c7bd16898529b77be1b1c73c1c4779a3a1e3bb71
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-09-12 10:35:32 +0200

    port models and method to API 8.0

--- logistic_order/model/sale_order.py
+++ logistic_order/model/sale_order.py
@@ -17,54 +17,53 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class sale_order(orm.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    _columns = {
-        # override only to change the 'string' argument
-        # from 'Customer' to 'Requesting Entity'
-        'partner_id': fields.many2one(
-            'res.partner',
-            'Requesting Entity',
-            readonly=True,
-            states={'draft': [('readonly', False)],
-                    'sent': [('readonly', False)]},
-            required=True,
-            change_default=True,
-            select=True,
-            track_visibility='always'),
-        'consignee_id': fields.many2one(
-            'res.partner',
-            string='Consignee',
-            required=True),
-        'incoterm_address': fields.char(
-            'Incoterm Place',
-            help="Incoterm Place of Delivery. "
-                 "International Commercial Terms are a series of "
-                 "predefined commercial terms used in "
-                 "international transactions."),
-        'delivery_time': fields.char('Delivery time'),
-        # TODO Move this in translation, as all others stuff like it !
-        'state': fields.selection([
-            ('draft', 'Draft Cost Estimate'),
-            ('sent', 'Cost Estimate Sent'),
-            ('cancel', 'Cancelled'),
-            ('waiting_date', 'Waiting Schedule'),
-            ('progress', 'Logistic Order'),
-            ('manual', 'Logistic Order to Invoice'),
-            ('shipping_except', 'Shipping Exception'),
-            ('invoice_except', 'Invoice Exception'),
-            ('done', 'Done')],
-            'Status',
-            readonly=True,
-            track_visibility='onchange',
-            help="Gives the status of the cost estimate or logistic order.\n"
-                 "The exception status is automatically set when a cancel"
-                 " operation occurs in the processing of a document linked to the logistic order.\n"
-                 "The 'Waiting Schedule' status is set when the invoice is"
-                 " confirmed but waiting for the scheduler to run on the order date.",
-            select=True),
-    }
+    # override only to change the 'string' argument
+    # from 'Customer' to 'Requesting Entity'
+    partner_id = fields.Many2one(
+        'res.partner',
+        'Requesting Entity',
+        readonly=True,
+        states={'draft': [('readonly', False)],
+                'sent': [('readonly', False)]},
+        required=True,
+        change_default=True,
+        select=True,
+        track_visibility='always')
+    consignee_id = fields.Many2one(
+        'res.partner',
+        string='Consignee',
+        required=True)
+    incoterm_address = fields.Char(
+        'Incoterm Place',
+        help="Incoterm Place of Delivery. "
+             "International Commercial Terms are a series of "
+             "predefined commercial terms used in "
+             "international transactions.")
+    delivery_time = fields.Char('Delivery time')
+    # TODO Move this in translation, as all others stuff like it !
+    state = fields.Selection([
+        ('draft', 'Draft Cost Estimate'),
+        ('sent', 'Cost Estimate Sent'),
+        ('cancel', 'Cancelled'),
+        ('waiting_date', 'Waiting Schedule'),
+        ('progress', 'Logistic Order'),
+        ('manual', 'Logistic Order to Invoice'),
+        ('shipping_except', 'Shipping Exception'),
+        ('invoice_except', 'Invoice Exception'),
+        ('done', 'Done')],
+        'Status',
+        readonly=True,
+        track_visibility='onchange',
+        help="Gives the status of the cost estimate or logistic order.\n"
+             "The exception status is automatically set when a cancel "
+             "operation occurs in the processing of a document linked to the "
+             "logistic order.\nThe 'Waiting Schedule' status is set when the "
+             "invoice is confirmed but waiting for the scheduler to run on the"
+             "order date.",
+        select=True)
