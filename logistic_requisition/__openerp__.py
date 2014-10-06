b72e176d8160f4eddc0fc42cd5573c2e92e072bf
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-10-06 17:07:16 +0200

    improve manifest

--- logistic_requisition/__openerp__.py
+++ logistic_requisition/__openerp__.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
 #
 #
 #    Author: Joël Grand-Guillaume, Jacques-Etienne Baudoux, Guewen Baconnier
 #    Copyright 2013-2014 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more description.
+#    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+#
 
 {"name": "Logistic Requisition",
  "version": "0.2",
@@ -32,41 +31,53 @@
 This module allows you to manage your Logistic Requisitions.
 ============================================================
 
-A Logistic requisition express a need that is requested somewhere. It allows to 
-manage the sourcing of the needs before making a cost estimate to the requestor.
+A Logistic requisition express a need that is requested somewhere.
+It allows to manage the sourcing of the needs before making a cost estimate to
+the requestor.
 
-This invert the logic that is in standard in OpenERP in the way that the sourcing 
-of the procuremnt is made before the order confirmation and we built the link incase 
-of validation. In standard, th SO confirmation generate the procurement then it
-generate the needed procurement.
+This invert the logic that is in standard in Odoo in the way that the sourcing
+of the procuremnt is made before the order confirmation and we built the link
+incase of validation. In standard, th SO confirmation generate the procurement
+then it generate the needed procurement.
 
 The sourcing can be of various type:
- 
- * Warehouse dispatch: You will deliver the requested goods from one of your 
+
+ * Warehouse dispatch: You will deliver the requested goods from one of your
  stock location
- * Procurement : You will go for tender to chose the best supplier for the 
- requiredgoods
- * Framework agreement : You will use the existing Framework agreement
- * Other : Nothing of those choices. In that case, the line can be selected
+ * Procurement: You will go for tender to chose the best supplier for the
+ required goods
+ * Framework agreement: You will use the existing Framework agreement
+ * Other: Nothing of those choices. In that case, the line can be selected
 and included in either a PO or a tender
 
 The simple process is the following:
 
- * LR are recorded as the to represent the need of the customer/requestor
- * LR are confirm and sourced of different way (dispatch, tender, ..)
- * If sourced from tender, you need to go all along the tendering process (CBA).
- Once you chose one bid, the system will automatically put back the prices 
+ * LR are recorded to represent the need of the customer/requestor
+ * LR are confirmed and sourced by different way (dispatch, tender, ..)
+ * If sourced from tender, you need to go all along the tendering process
+   (CBA).
+ Once you chose one bid, the system will automatically put back the prices
  information on the sourcing line of the LR.
-  * You can create a cost estimate from sourced LR lines for your 
+  * You can create a cost estimate from sourced LR lines for your
  resquestor/customer
- * If the requestor accept the cost estimate, his validation will automatically:
-   * Create the link between the chosen winning bid and the line sourced this way. 
-     A new draft PO will be generatedand managed as drop shipping.
-   * Lines sourced as a dispatch will create a delivery order. 
+ * If the requestor accepts the cost estimate, his validation will
+   automatically:
+   * Create the link between the chosen winning bid and the line sourced this
+     way. A new draft PO will be generatedand managed as drop shipping.
+   * Lines sourced as a dispatch will create a delivery order.
    * Lines sourced a LTA will create a PO of type LTA
+
+
+Contributors
+------------
+
+* Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
+* Jacques-Etienne Baudoux <je@bcim.be>
+* Guewen Baconnier <guewen.baconnier@camptocamp.com>
+* Nicolas Bessi <nicolas.bessi@camptocamp.com>
+* Yannick Vaucher <yannick.vaucher@camptocamp.com>
 """,
- "depends": [
-             "stock_dropshipping",
+ "depends": ["stock_dropshipping",
              "purchase",
              "purchase_requisition_extended",
              "mail",
