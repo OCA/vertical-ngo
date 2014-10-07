5470717fdba7863c8b7563d00bfc829fb89adc9a
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-10-07 12:00:41 +0200

    set string override in pot file

--- logistic_order/model/sale_order.py
+++ logistic_order/model/sale_order.py
@@ -24,18 +24,6 @@ from openerp import SUPERUSER_ID
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # override only to change the 'string' argument
-    # from 'Customer' to 'Requesting Entity'
-    partner_id = fields.Many2one(
-        'res.partner',
-        'Requesting Entity',
-        readonly=True,
-        states={'draft': [('readonly', False)],
-                'sent': [('readonly', False)]},
-        required=True,
-        change_default=True,
-        select=True,
-        track_visibility='always')
     consignee_id = fields.Many2one(
         'res.partner',
         string='Consignee',
@@ -47,27 +35,6 @@ class SaleOrder(models.Model):
              "predefined commercial terms used in "
              "international transactions.")
     delivery_time = fields.Char('Delivery time')
-    # TODO Move this in translation, as all others stuff like it !
-    state = fields.Selection([
-        ('draft', 'Draft Cost Estimate'),
-        ('sent', 'Cost Estimate Sent'),
-        ('cancel', 'Cancelled'),
-        ('waiting_date', 'Waiting Schedule'),
-        ('progress', 'Logistic Order'),
-        ('manual', 'Logistic Order to Invoice'),
-        ('shipping_except', 'Shipping Exception'),
-        ('invoice_except', 'Invoice Exception'),
-        ('done', 'Done')],
-        'Status',
-        readonly=True,
-        track_visibility='onchange',
-        help="Gives the status of the cost estimate or logistic order.\n"
-             "The exception status is automatically set when a cancel "
-             "operation occurs in the processing of a document linked to the "
-             "logistic order.\nThe 'Waiting Schedule' status is set when the "
-             "invoice is confirmed but waiting for the scheduler to run on the"
-             "order date.",
-        select=True)
 
     @api.cr
     def init(self, cr):
