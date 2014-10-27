71eb25727309bf3b933e2e3bbc14a62fdad9005f
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-27 16:07:24 +0100

    improve doc, move it to README.rst

---
+++ framework_agreement_requisition/README.rst
@@ -0,0 +1,21 @@
+Negociate framework agreement in the Tender
+===========================================
+
+This module allows you to negociate a framework agreement in the Tender.
+
+To to so you have to check the box "Negociate Agreement".
+
+The module add a state "Agreement selected" on the tender and on the Purchase
+Order.
+
+These will be the final state once you have chosen the agreement that best fits
+your needs.
+
+Once the selection is done just use the button "Agreement selected" on the
+tender. That will close the flow of tender and the related PO accordingly.
+
+Future Improvements
+===================
+
+The generated framework agreement does not contain the negociated prices.
+Those need to be filled in manually.
--- framework_agreement_requisition/__openerp__.py
+++ framework_agreement_requisition/__openerp__.py
@@ -27,24 +27,6 @@
  'depends': ['purchase_requisition',
              'purchase_requisition_bid_selection',
              'framework_agreement'],
- 'description': """
-Negociate framework agreement in the Tender
-===========================================
-
-This module allows you to negociate a framework agreement in the Tender.
-
-To to so you have to check the box "Negociate Agreement".
-
-The module add a state "Agreement selected" on the tender and on the Purchase
-Order.
-
-These will be the final state once you have chosen the agreement that best fits
-your needs.
-
-Once the selection is done just use the button "Agreement selected" on the
-tender. That will close the flow of tender and the related PO accordingly.
-
-""",
  'website': 'http://www.camptocamp.com',
  'data': ['requisition_workflow.xml',
           'purchase_workflow.xml',
