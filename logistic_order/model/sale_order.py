46b98dc4ca6c56194ec0c1e990f291d297b03572
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-11-07 14:28:36 +0100

    Add auto link of SO lines to stock.location.route.
    Based on SO line field `sourced_by`

--- logistic_order/model/sale_order.py
+++ logistic_order/model/sale_order.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class SaleOrder(models.Model):
@@ -30,3 +30,66 @@ class SaleOrder(models.Model):
              "predefined commercial terms used in "
              "international transactions.")
     delivery_time = fields.Char('Delivery time')
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def _get_po_location_usage(self, purchase_order_line):
+        """Retrieve the destination location usage of a PO
+        from a PO line
+
+        :param purchase_order_line: record of `purchase.order.line` Model
+        :type purchase_order_line: :py:class:`openerp.models.Model`
+        purchase_order = purchase_order_line.order_id
+
+        :return: PO location usage
+        :rtype: str
+
+        """
+        return purchase_order_line.order_id.location_id.usage
+
+    @api.model
+    def _route_from_usage(self, usage):
+        """Return the routes to assing on SO lines
+        based on a location usage.
+
+        If nothing no match return None
+
+        :param usage: stock.location Model usage
+        :type usage: str
+
+        :return: a record of `stock.location.route`
+        :rtype: :py:class:`openerp.models.Model` or None
+        """
+        if usage == 'customer':
+            return self.env.ref('stock_dropshipping.route_drop_shipping')
+        elif usage == 'internal':
+            return self.env.ref('stock.route_warehouse0_mto')
+        else:
+            return None
+
+    @api.one
+    @api.onchange('sourced_by')
+    @api.constrains('sourced_by')
+    def set_route_form_so(self):
+        """Set route on SO line based on fields sourced_by.
+
+        Wee look for the PO related
+        to current SO line by the sourced_by fields.
+
+        If the PO has a destination location with usage
+        "customer" we apply the dropshipping route to current SO line.
+
+        If the PO has a destination location with usage
+        "internal" we apply the make to order route to current SO line.
+
+        As there is no trigger decorator that works on
+        non computed fields we use constrains decorator instead.
+        """
+        if not self.sourced_by:
+            return
+        usage = self._get_po_location_usage(self.sourced_by)
+        route = self._route_from_usage(usage)
+        self.route_id = route
