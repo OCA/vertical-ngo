70a59ca651fd0a957dff2320c97aff106e605645
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-11-10 16:13:08 +0100

    Bump version

--- logistic_order/__openerp__.py
+++ logistic_order/__openerp__.py
@@ -20,7 +20,7 @@
 
 {"name": "Logistics Order",
  "summary": "Sales Order as Logistics Order",
- "version": "0.1",
+ "version": "0.2",
  "author": "Camptocamp",
  "license": "AGPL-3",
  "category": "Purchase Management",
