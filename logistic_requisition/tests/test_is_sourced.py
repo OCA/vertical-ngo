00e972972cc5f2c6d8fcc5bacacc9f00a3537bc3
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-11-12 15:46:15 +0100

    remove unused imports

--- logistic_requisition/tests/test_is_sourced.py
+++ logistic_requisition/tests/test_is_sourced.py
@@ -13,9 +13,7 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import time
 from openerp.tests.common import TransactionCase
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT as D_FMT
 
 
 class TestIsSourced(TransactionCase):
