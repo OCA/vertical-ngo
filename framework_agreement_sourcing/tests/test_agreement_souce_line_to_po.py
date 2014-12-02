cf696cb25f96983bde7cab0d829742d1ecbf1742
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-12-02 11:52:58 +0100

    Add tests on picking_type for PO creation from FA

--- framework_agreement_sourcing/tests/test_agreement_souce_line_to_po.py
+++ framework_agreement_sourcing/tests/test_agreement_souce_line_to_po.py
@@ -28,16 +28,16 @@ class TestSourceToPo(CommonSourcingSetUp):
         super(TestSourceToPo, self).setUp()
         cr, uid = self.cr, self.uid
         lines = self.requisition.line_ids
-        agr_line = None
+        self.agr_line = None
         self.wiz_model = self.registry(
             'logistic.requisition.source.create.agr.po')
         for line in lines:
             if line.product_id == self.cheap_on_low_agreement.product_id:
-                agr_line = line
+                self.agr_line = line
                 break
-        self.assertTrue(agr_line, 'no agr line found')
-        agr_line.write({'requested_qty': 400})
-        agr_line.refresh()
+        self.assertTrue(self.agr_line, 'no agr line found')
+        self.agr_line.write({'requested_qty': 400})
+        self.agr_line.refresh()
         source_ids = []
         for line in lines:
             if (line.product_id.id == self.product_id or
@@ -52,9 +52,21 @@ class TestSourceToPo(CommonSourcingSetUp):
         self.other_source = next(x for x in self.source_lines
                                  if x.procurement_method == 'other')
 
-    def test_01_transform_source_to_agreement(self):
+    def test_01_transform_source_to_agreement_wh_dest(self):
         """Test transformation of an agreement source line into PO"""
         cr, uid = self.cr, self.uid
+
+        # Set destination address
+        partner_vals = {
+            'name': 'Warehouse address',
+            }
+        partner_wh = self.env['res.partner'].create(partner_vals)
+        warehouse_id = self.ref('stock.warehouse0')
+        warehouse = self.env['stock.warehouse'].browse(warehouse_id)
+        warehouse.partner_id = partner_wh
+        picking_type_wh = self.ref('stock.picking_type_in')
+        self.agr_line.consignee_shipping_id = partner_wh
+
         self.assertTrue(self.lta_source, 'no lta source found')
         self.lta_source.refresh()
         active_ids = [x.id for x in self.source_lines]
@@ -76,6 +88,66 @@ class TestSourceToPo(CommonSourcingSetUp):
             po.pricelist_id, supplier.property_product_pricelist_purchase)
         self.assertEqual(po.date_order, date_order)
         self.assertEqual(po.dest_address_id, add)
+        self.assertEqual(po.picking_type_id.id, picking_type_wh)
+        self.assertEqual(po.consignee_id, consignee)
+        self.assertEqual(po.state, 'draftpo')
+        self.assertNotEqual(po.name, self.requisition.name)
+        self.assertEqual(len(po.order_line), 2)
+
+        po_line = next(x for x in po.order_line
+                       if x.product_id ==
+                       self.lta_source.framework_agreement_id.product_id)
+        self.assertEqual(po_line.product_qty, self.lta_source.proposed_qty)
+        self.assertEqual(
+            po_line.product_id, self.lta_source.proposed_product_id)
+        self.assertEqual(po_line.product_qty, self.lta_source.proposed_qty)
+        self.assertEqual(po_line.product_uom, self.lta_source.proposed_uom_id)
+        self.assertAlmostEqual(po_line.price_unit, 50.0)
+        self.assertEqual(po_line.lr_source_line_id, self.lta_source)
+        self.assertEqual(po_line.date_planned, date_delivery)
+
+        po_line = next(x for x in po.order_line
+                       if x.product_id ==
+                       self.other_source.proposed_product_id)
+        self.assertEqual(po_line.product_qty, self.other_source.proposed_qty)
+        self.assertEqual(
+            po_line.product_id, self.other_source.proposed_product_id)
+        self.assertEqual(po_line.product_qty, self.other_source.proposed_qty)
+        self.assertEqual(
+            po_line.product_uom, self.other_source.proposed_uom_id)
+        self.assertAlmostEqual(po_line.price_unit, 1.0)
+        self.assertEqual(po_line.lr_source_line_id, self.other_source)
+
+    def test_01_transform_source_to_agreement_dropshipping(self):
+        """Test transformation of an agreement source line into PO"""
+        cr, uid = self.cr, self.uid
+
+        # Set destination address
+        partner_dropship = self.ref('base.res_partner_12')
+        self.agr_line.consignee_shipping_id = partner_dropship
+
+        self.assertTrue(self.lta_source, 'no lta source found')
+        self.lta_source.refresh()
+        active_ids = [x.id for x in self.source_lines]
+        wiz_id = self.wiz_model.create(self.cr, self.uid, {},
+                                       context={'active_ids': active_ids})
+
+        po_id = self.wiz_model.action_create_agreement_po_requisition(
+            cr, uid, [wiz_id], context={'active_ids': active_ids}
+        )['res_id']
+        self.assertTrue(po_id, "no PO created")
+        supplier = self.lta_source.framework_agreement_id.supplier_id
+        add = self.lta_source.requisition_id.consignee_shipping_id
+        consignee = self.lta_source.requisition_id.consignee_id
+        po = self.registry('purchase.order').browse(cr, uid, po_id)
+        date_order = self.lta_source.requisition_id.date
+        date_delivery = self.lta_source.requisition_id.date_delivery
+        self.assertEqual(po.partner_id, supplier)
+        self.assertEqual(
+            po.pricelist_id, supplier.property_product_pricelist_purchase)
+        self.assertEqual(po.date_order, date_order)
+        self.assertEqual(po.dest_address_id, add)
+        self.assertEqual(po.picking_type_id.name, 'Dropship')
         self.assertEqual(po.consignee_id, consignee)
         self.assertEqual(po.state, 'draftpo')
         self.assertNotEqual(po.name, self.requisition.name)
