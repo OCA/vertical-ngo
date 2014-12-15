18d2b27b1cf95e4b3e6fd419afcf3c60787edf67
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-12-15 16:04:17 +0100

    use self.env.user

--- logistic_requisition_department/model/logistic_requisition.py
+++ logistic_requisition_department/model/logistic_requisition.py
@@ -22,8 +22,7 @@ class LogisticRequisition(models.Model):
     _inherit = 'logistic.requisition'
 
     def _get_my_department(self):
-        my_user = self.env['res.users'].browse(self.env.uid)
-        return my_user.employee_ids[0].department_id
+        return self.env.user.employee_ids[0].department_id
 
     department_id = fields.Many2one('hr.department', 'Department',
                                     default=_get_my_department)
