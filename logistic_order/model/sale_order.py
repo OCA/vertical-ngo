2284b025eb6046ac87b246e582f612bec908809b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-01-21 16:43:02 +0100

    Add state 'accepted' on logistic.order We add it between 'draft,sent' and 'confirmed'

--- logistic_order/model/sale_order.py
+++ logistic_order/model/sale_order.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright 2013-2014 Camptocamp SA
+#    Copyright 2013-2015 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -28,6 +28,27 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     LO_STATES = base_sale_order.LO_STATES
+
+    state = fields.Selection([
+        ('draft', 'Draft Quotation'),
+        ('sent', 'Quotation Sent'),
+        # Added state
+        ('accepted', 'Cost Estimate Accepted'),
+        ('cancel', 'Cancelled'),
+        ('waiting_date', 'Waiting Schedule'),
+        ('progress', 'Sales Order'),
+        ('manual', 'Sale to Invoice'),
+        ('shipping_except', 'Shipping Exception'),
+        ('invoice_except', 'Invoice Exception'),
+        ('done', 'Done'),
+        ], 'Status', readonly=True, copy=False,
+        help="Gives the status of the quotation or sales order.\nThe exception"
+             " status is automatically set when a cancel operation occurs in "
+             "the invoice validation (Invoice Exception) or in the picking "
+             "list process (Shipping Exception).\nThe 'Waiting Schedule' "
+             "status is set when the invoice is confirmed but waiting for the "
+             "scheduler to run on the order date.",
+        select=True)
 
     # incoterm is overridden to add states
     incoterm = fields.Many2one(
