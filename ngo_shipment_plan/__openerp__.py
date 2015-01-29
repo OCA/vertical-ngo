1229c82b16c9aecf36da8a9f2ca1173e31c4b458
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-01-29 12:06:45 +0100

    Add ngo_shipment_management

---
+++ ngo_shipment_plan/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    Authors: Yannick Vaucher
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more description.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{"name": "NGO - Shipment Management",
+ "summary": "Adaptations of Shipment Management for NGO",
+ "version": "0.1",
+ "author": "Camptocamp",
+ "category": "NGO",
+ "license": 'AGPL-3',
+ "complexity": "normal",
+ "images": [],
+ "website": "http://www.camptocamp.com",
+ "depends": ["logistic_order",
+             "stock_shipment_management",
+             ],
+ "demo": [],
+ "data": ["view/menu.xml",
+          ],
+ "auto_install": False,
+ "test": [],
+ "installable": True,
+ }
