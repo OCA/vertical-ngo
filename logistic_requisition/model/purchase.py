01934eb9ddd2fce6fb3a0a4eb68de486f1e1977f
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-04-07 14:57:02 +0200

    [TYPO]

--- logistic_requisition/model/purchase.py
+++ logistic_requisition/model/purchase.py
@@ -118,7 +118,7 @@ class PurchaseOrderLine(models.Model):
                         raise exceptions.Warning(
                             _("You cannot change the informations because "
                               "this PO line is already linked to a Logistics "
-                              "Requsition Line %s marked as sourced or "
+                              "Requisition Line %s marked as sourced or "
                               "quoted.") % (line.lr_source_line_id.name)
                         )
                     else:
@@ -135,7 +135,7 @@ class PurchaseOrderLine(models.Model):
                 if (source_line.requisition_line_id in ('sourced', 'quoted')):
                     raise Warning(
                         _("You cannot delete this PO line because it is "
-                          "already linked to a Logistics Requsition Line %s "
+                          "already linked to a Logistics Requisition Line %s "
                           "marked as sourced or quoted.")
                         % (line.lr_source_line_id.name)
                     )
