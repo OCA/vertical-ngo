c9625211788f56b254111637cd31bc728603d49d
Joël Grand-Guillaume <joel.grandguillaume@gmail.com>
2015-05-04 14:05:09 +0200

    [FIX] Shortcut button from LR to open related PO

--- logistic_requisition/model/logistic_requisition.py
+++ logistic_requisition/model/logistic_requisition.py
@@ -340,7 +340,7 @@ class LogisticsRequisition(models.Model):
     @api.multi
     def _get_relevant_purchases(self):
         sourced_lines = self.line_ids.filtered(
-            lambda rec: rec.state == 'Sourced'
+            lambda rec: rec.state in ('Sourced', 'Quoted')
             )
         purchases = sourced_lines.mapped(
             'source_ids.purchase_line_id.order_id'
