22da959e3be03cfdf084a410e448f447ee441ee5
Shobha Padasalagi <shobha.p.serpentcs@gmail.com>
2017-07-18 19:23:00 +0530

    [IMP]NGO Purchase and NGO Purchase Requisition modules Improved License files and README.rst files.

--- ngo_purchase/__manifest__.py
+++ ngo_purchase/__manifest__.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-{'name': 'NGO - Purchase Order',
- 'summary': 'Base Purchase Order view for NGO',
- 'version': '10.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA), \
+{
+    'name': 'NGO - Purchase Order',
+    'summary': 'Base Purchase Order view for NGO',
+    'version': '10.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA), \
          Serpent Consulting Services Pvt. Ltd.',
- 'license': 'AGPL-3',
- 'category': 'Purchase Management',
- 'complexity': 'normal',
- 'website': 'http://www.camptocamp.com',
- 'depends': [
-             'framework_agreement',
-             'framework_agreement_requisition',
-             'purchase_origin_address',
-             'purchase_delivery_address',
-             'purchase_requisition',
-             'purchase_requisition_bid_selection',
-             'purchase_rfq_bid_workflow',
-             'purchase_transport_document',
-             ],
- 'data': [
-          'view/purchase_order.xml',
-          ],
- 'installable': True,
- 'auto_install': False,
- }
+    'category': 'Purchase Management',
+    'complexity': 'normal',
+    'website': 'http://www.camptocamp.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'framework_agreement',
+        'framework_agreement_requisition',
+        'purchase_origin_address',
+        'purchase_delivery_address',
+        'purchase_requisition',
+        'purchase_requisition_bid_selection',
+        'purchase_rfq_bid_workflow',
+        'purchase_transport_document',
+    ],
+    'data': [
+        'views/purchase_order.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
--- ngo_purchase_requisition/__manifest__.py
+++ ngo_purchase_requisition/__manifest__.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
-{'name': 'NGO - Purchase Requisition',
- 'summary': 'Base Purchase Requisition view for NGO',
- 'version': '10.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA),\
-         Serpent Consulting Services Pvt. Ltd.',
- 'license': 'AGPL-3',
- 'category': 'Purchase Management',
- 'complexity': 'normal',
- 'website': 'http://www.camptocamp.com',
- 'depends': ['purchase_requisition',
-             'purchase_requisition_bid_selection',
-             'purchase_requisition_delivery_address',
-             'purchase_requisition_auto_rfq',
-             'purchase_requisition_transport_document',
-             ],
- 'data': ['view/purchase_order.xml',
-          'view/purchase_requisition.xml',
-          ],
- 'installable': True,
- 'auto_install': False,
+{
+    'name': 'NGO - Purchase Requisition',
+    'summary': 'Base Purchase Requisition view for NGO',
+    'version': '10.0.1.0.0',
+    'category': 'Purchase Management',
+    'author': 'Camptocamp,Odoo Community Association (OCA),\
+            Serpent Consulting Services Pvt. Ltd.',
+    'complexity': 'normal',
+    'website': 'http://www.camptocamp.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'purchase_requisition',
+        'purchase_requisition_bid_selection',
+        'purchase_requisition_delivery_address',
+        'purchase_requisition_auto_rfq',
+        'purchase_requisition_transport_document',
+    ],
+    'data': [
+        'views/purchase_order.xml',
+        'views/purchase_requisition.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
 }
